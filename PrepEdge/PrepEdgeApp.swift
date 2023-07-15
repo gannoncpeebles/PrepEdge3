43de41a11d9e8448bb66cab799a837ad8903a5a3
Gannon Peebles <gannoncpeebles@gmail.com>
2023-07-14 23:43:17 -0400

    Initial Commit

---
+++ PrepEdge/PrepEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepEdgeApp.swift
+//  PrepEdge
+//
+//  Created by Gannon Peebles on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PrepEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
